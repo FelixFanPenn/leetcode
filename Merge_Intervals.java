8ec48bf6de905be5e753b56eb7c4e10ca5dbc03b
FelixFanPenn <fankejin@seas.upenn.edu>
2016-10-09 11:12:57 -0400

    java solution for Merge_Intervals

---
+++ Merge_Intervals.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> res = new LinkedList<>();
+        if (intervals.size() == 0) return res;
+        Collections.sort(intervals, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval inter1, Interval inter2){
+                return inter1.start - inter2.start;
+            }
+        });
+        
+        int start = intervals.get(0).start, end = intervals.get(0).end;
+        
+        for (Interval inter : intervals){
+            if (inter.start <= end){
+                end = Math.max(end, inter.end);
+            } else {
+                Interval tmp = new Interval(start, end);
+                res.add(tmp);
+                start = inter.start;
+                end = inter.end;
+            }
+        }
+        res.add(new Interval(start, end));
+        return res;
+    }
+}
