fbebfc35fda1434e1142b620370d955b8918aabc
FelixFanPenn <fankejin@seas.upenn.edu>
2016-10-20 19:44:30 -0400

    Update Contains_Duplicate.java

--- Contains_Duplicate.java
+++ Contains_Duplicate.java
@@ -1,11 +1,11 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         if (nums.length == 0) return false;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Set<Integer> set = new HashSet<Integer>();
         
         for (int i = 0; i < nums.length; i++){
-            if (map.containsKey(nums[i])) return true;
-            map.put(nums[i], i);
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
         }
         return false;
     }
