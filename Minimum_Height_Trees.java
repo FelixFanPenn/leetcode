4f29b49afebc91d3442cf5383004d98a907834fc
FelixFanPenn <fankejin@seas.upenn.edu>
2016-11-02 20:53:31 -0400

    java solution for Minimum_Height_Trees

---
+++ Minimum_Height_Trees.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) return Arrays.asList(0);
+        List<Set<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new HashSet<>());
+        
+        for (int i = 0; i < edges.length; i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < adj.size(); i++){
+            if (adj.get(i).size() == 1) leaves.add(i);
+        }
+        
+        while (n > 2){
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int i : leaves){
+                int j = adj.get(i).iterator().next();
+                adj.get(j).remove(i);
+                if (adj.get(j).size() == 1) newLeaves.add(j);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
+    }
+}
