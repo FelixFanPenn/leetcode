4376a05da2c3032bc9dbbfe792ca4ec57159e56a
FelixFanPenn <fankejin@seas.upenn.edu>
2016-11-09 14:21:08 -0500

    java solution for Move_Zeroes

---
+++ Move_Zeroes.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0, right = 0;
+        while(right < nums.length){
+            if (nums[right] != 0){
+                nums[left++] = nums[right];
+            }
+            right++;
+        }
+        while(left < nums.length){
+            nums[left++] = 0;
+        }
+    }
+}
