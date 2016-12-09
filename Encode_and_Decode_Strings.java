d7b2cb29d4c73559a3b2cac72e6319b0722e0ad3
FelixFanPenn <fankejin@seas.upenn.edu>
2016-12-09 17:09:55 -0500

    java solution for Encode_and_Decode_Strings

---
+++ Encode_and_Decode_Strings.java
@@ -0,0 +1,28 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs){
+            sb.append(str.length()).append('/').append(str);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        while (i < s.length()){
+            int index = s.indexOf('/', i);
+            int size = Integer.valueOf(s.substring(i, index));
+            ret.add(s.substring(index+1, index+size+1));
+            i = index + size + 1;
+        }
+        return ret;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
