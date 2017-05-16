287ed109f663575442283a3030aa5431d56da025
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-16 17:48:46 -0400

    java solution for Insertion_Sort_List

--- Insertion_Sort_List.java
+++ Insertion_Sort_List.java
@@ -6,6 +6,51 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode newHead = new ListNode(0);
+        
+        while (head != null){
+            int val = head.val;
+            
+            ListNode itr = newHead.next;
+            ListNode pre = newHead;
+            
+            while (itr != null && itr.val < val){
+                pre = pre.next;
+                itr = itr.next;
+            }
+            
+            
+            ListNode node = new ListNode(val);
+            pre.next = node;
+            if (itr != null){
+                node.next = itr;
+            }
+            
+            head = head.next;
+        }
+        
+        return newHead.next;
+    }
+}
+
+
+
+/*
 public class Solution {
     public ListNode insertionSortList(ListNode head) {
         if (head == null || head.next == null) return head;
@@ -24,3 +69,4 @@ public class Solution {
         return newhead.next;
     }
 }
+*/
