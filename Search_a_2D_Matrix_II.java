72fdc96d59269206ea2492a0a26359c97a98bd87
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-16 12:01:34 -0400

    java solution for Search_a2D_Matrix_2

--- Search_a_2D_Matrix_II.java
+++ Search_a_2D_Matrix_II.java
@@ -1,5 +1,134 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        
+        int startRow = findStartRow(matrix, target);
+        int endRow = findEndRow(matrix, target);
+        int startCol = findStartCol(matrix, target);
+        int endCol = findEndCol(matrix, target);
+        
+        if (startRow == -1 || endRow == -1 || startCol == -1 || endCol == -1) {
+            return true;
+        }
+        
+        if (startRow > endRow || startCol > endCol) {
+            return false;
+        }
+        
+        for (int i = startRow; i <= endRow ; i++){
+            int s = startCol; int e = endCol;
+            while (s + 1 < e){
+                int mid = (s + e) /2;
+                
+                if (matrix[i][mid] == target){
+                    return true;
+                } else if (matrix[i][mid] > target){
+                    e = mid;
+                } else {
+                    s = mid;
+                }
+            }
+            
+            if (matrix[i][s] == target || matrix[i][e] == target){
+                return true;
+            }
+        }
+        return false;
+    
+    }
+    
+    public int findStartRow(int[][] matrix, int target){
+        int col = matrix[0].length - 1;
+        int start = 0; int end = matrix.length - 1;
+        
+        while (start + 1 < end){
+            int mid = (start + end) / 2;
+            if (matrix[mid][col] == target){
+                return -1;
+            } else if (matrix[mid][col] > target){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (matrix[start][col] >= target){
+            return start;
+        }
+        return end;
+        
+    }
+    
+    public int findEndRow(int[][] matrix, int target){
+        int start = 0; int end = matrix.length - 1;
+        
+        while (start + 1 < end){
+            int mid = (start + end) / 2;
+            if (matrix[mid][0] == target){
+                return -1;
+            } else if (matrix[mid][0] > target){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (matrix[end][0] <= target){
+            return end;
+        }
+        return start;
+    }
+    
+    public int findStartCol(int[][] matrix, int target){
+        int row = matrix.length - 1;
+        int start = 0; int end = matrix[0].length - 1;
+        
+        
+        while (start + 1 < end){
+            int mid = (start + end) / 2;
+            if (matrix[row][mid] == target){
+                return -1;
+            } else if (matrix[row][mid] > target){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (matrix[row][start] >= target){
+            return start;
+        }
+        return end;
+    }
+    
+    public int findEndCol(int[][] matrix, int target){
+        int start = 0; int end = matrix[0].length - 1;
+        
+        
+        while (start + 1 < end){
+            int mid = (start + end) / 2;
+            if (matrix[0][mid] == target){
+                return -1;
+            } else if (matrix[0][mid] > target){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (matrix[0][end] <= target){
+            return end;
+        }
+        return start;
+    }
+}
+
+
+/*
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix.length == 0) return false;
         int row = matrix.length - 1;
         int col = matrix[0].length - 1;
@@ -53,3 +182,4 @@ public class Solution {
         return false;
     }
 }
+*/
