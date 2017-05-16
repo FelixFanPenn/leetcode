ea9ca068517ec8b2a4e43d5711cb8b5bea44993e
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-16 11:55:37 -0400

    add two numbers

--- Add_Two_Numbers.java
+++ Add_Two_Numbers.java
@@ -8,44 +8,34 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) return null;
         if (l1 == null) return l2;
         if (l2 == null) return l1;
         int carry = 0;
         ListNode head = new ListNode(0);
         ListNode itr = head;
-        
-        while(l2 != null && l1 != null){
-            int tmp = l1.val + l2.val + carry;
-            ListNode node = new ListNode(tmp % 10);
+        while (l1 != null || l2 != null){
+            int i = 0;
+            int j = 0;
+            if (l1 != null) {
+                i = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                j = l2.val;
+                l2 = l2.next;
+            }
+            ListNode node = new ListNode((i+j+carry) % 10);
             itr.next = node;
             itr = itr.next;
-            carry = tmp/10;
-            l2 = l2.next;
-            l1 = l1.next;
+            
+            carry = (i+j+carry) / 10;
         }
         
-        while (l1 != null){
-            int tmp = l1.val + carry;
-            ListNode node = new ListNode(tmp % 10);
+        if (carry != 0){
+            ListNode node = new ListNode(carry);
             itr.next = node;
-            itr = itr.next;
-            carry = tmp/10;
-            l1 = l1.next;
-        }
-        
-        while (l2 != null){
-            int tmp = l2.val + carry;
-            ListNode node = new ListNode(tmp % 10);
-            itr.next = node;
-            itr = itr.next;
-            carry = tmp/10;
-            l2 = l2.next;
-        }
-        if (carry != 0) {
-             ListNode node = new ListNode(carry);
-             itr.next = node;
         }
         return head.next;
+        
     }
 }
