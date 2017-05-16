b8dc3eb472abf9e6f1bbbdce6fdf4293ac493482
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-16 14:25:09 -0400

    java solution for Odd_Even_Linked_List

--- Odd_Even_Linked_List.java
+++ Odd_Even_Linked_List.java
@@ -1,3 +1,5 @@
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -6,6 +8,45 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        ListNode oddTail = odd;
+        
+        
+        while (even != null && even.next != null){
+            
+            oddTail.next = oddTail.next.next;
+            oddTail = oddTail.next;
+            
+            even.next = even.next.next;
+            even = even.next;
+        }
+        
+        oddTail.next = evenHead;
+        
+        return odd;
+    }
+    
+}
+
+/*
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) return head;
@@ -24,3 +65,4 @@ public class Solution {
         return head;
     }
 }
+*/
