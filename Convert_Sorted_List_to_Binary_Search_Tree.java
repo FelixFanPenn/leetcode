b4cef04a150dc99d1bedcfcd06e737db9f7478f6
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-17 09:45:18 -0400

    java solution for Convert_Sorted_List_to_Binary_Search_Tree

--- Convert_Sorted_List_to_Binary_Search_Tree.java
+++ Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -19,6 +19,34 @@ public class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         if (head == null) return null;
         
+        ListNode pre = null;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            pre = slow;
+            slow = slow.next;
+        }
+        
+        TreeNode node = new TreeNode(slow.val);
+        if (pre != null) {
+            pre.next = null;
+            node.left = sortedListToBST(head);
+        }
+        node.right = sortedListToBST(slow.next);
+        
+        return node;
+        
+    }
+    
+}
+
+/*
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        
         ListNode slow = head;
         ListNode fast = head.next;
         ListNode pre = null;
@@ -44,3 +72,4 @@ public class Solution {
 
     }
 }
+*/
