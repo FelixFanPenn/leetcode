e687abaa990ecdf8e01ba2413976262d4cdc2fd3
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-16 21:46:54 -0400

    java solution for Reverse_Linked_List_2

--- Reverse_Linked_List_2.java
+++ Reverse_Linked_List_2.java
@@ -6,6 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+/*
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null) return head;
@@ -32,5 +33,59 @@ public class Solution {
         first.next.next = cur;
         first.next = second;
         return itr.next;
+    }
+}
+*/
+
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || m == n) return head;
+        
+        ListNode head1 = new ListNode(0);
+        ListNode itr = head1;
+        itr.next = head;
+        ListNode tail1;
+        ListNode head2;
+        ListNode tail2;
+        ListNode head3;
+        int i = 1;
+        
+        while (m > i){
+            i++;
+            itr = itr.next;
+        }
+        
+        head2 = itr.next;
+        tail1 = itr;
+        tail1.next = null;
+        
+        int dif = n - m;
+        itr = head2;
+        
+        while (dif > 0 && itr != null){
+            dif--;
+            itr = itr.next;
+        }
+        
+        head3 = itr.next;
+        tail2 = itr;
+        tail2.next = null;
+        
+        reverse(head2);
+        
+        tail1.next = tail2;
+        head2.next = head3;
+        return head1.next;
+        
+    }
+    
+    public void reverse(ListNode head){
+        ListNode pre = null;
+        while (head != null){
+            ListNode tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
     }
 }
