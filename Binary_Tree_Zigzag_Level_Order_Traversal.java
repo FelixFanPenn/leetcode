136f299205e2d214c3066b242f3a078c2821316e
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-18 14:26:54 -0400

    java solution for Binary_Tree_Zigzag_Level_Order_Traversal

--- Binary_Tree_Zigzag_Level_Order_Traversal.java
+++ Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -9,24 +9,24 @@
  */
 public class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        help(res, root, 0);
-        int len = res.size();
-        for (int i = 0; i < len; i++){
-            if (i % 2 == 0) {
-                Collections.reverse(res.get(i));        // only even row should be reversed
-            }
-        }
-        return res;
+        List<List<Integer>> list = new ArrayList<>();
+        helper(root, list, 0);
+        return list;
     }
     
-    private void help(List<List<Integer>> res, TreeNode root, int level){
+    public void helper(TreeNode root, List<List<Integer>> list, int level){
         if (root == null) return;
-        if (res.size() == level){
-            res.add(new ArrayList<>());
+        if (level == list.size()) {
+            list.add(new ArrayList<>());
         }
-        res.get(level).add(root.val);
-        help(res, root.right, level+1);
-        help(res, root.left, level+1);
+        
+        if (level % 2 == 0){
+            list.get(level).add(root.val);
+        } else {
+            list.get(level).add(0, root.val);
+        }
+        
+        helper(root.left, list, level+1);
+        helper(root.right, list, level+1);
     }
 }
