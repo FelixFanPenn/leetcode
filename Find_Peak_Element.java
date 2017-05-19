3ca7cfaaa4b2567d70cbc9236f593f3b236fa384
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-19 16:08:53 -0400

    java solution for Find_Peak_Element

---
+++ Find_Peak_Element.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 0) return Integer.MIN_VALUE;
+        
+        int start = 0, end = nums.length-1;
+        while(start+1<end){
+            int mid = start + (end-start)/2;
+            if (nums[mid] > nums[mid-1]){
+                start = mid;
+            } else if (nums[mid] > nums[mid+1]){
+                end = mid;
+            } else start = mid;
+        }
+        if (nums[start] > nums[end]) return start;
+        else return end;
+    }
+}
