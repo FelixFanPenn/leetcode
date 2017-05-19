d91802e1cf0eee398ec3900204ea3042090fc76c
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-18 23:33:02 -0400

    java solution for Summary_Ranges

--- Summary_Ranges.java
+++ Summary_Ranges.java
@@ -1,5 +1,36 @@
 public class Solution {
     public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        int len = nums.length;
+        if (len == 0) return res;
+        
+        int pre = 0;
+        String tmp = "" + nums[0];
+
+        for (int i = 1; i < len; i++){
+            
+            if (nums[i] != nums[i-1] + 1) {
+                if (pre + 1 < i) {
+                    tmp = tmp + "->" + nums[i-1];
+                }
+                res.add(tmp);
+                pre = i;
+                tmp = "" + nums[i];
+            }
+        }
+        
+        if (pre < len-1){
+            tmp = tmp + "->" + nums[len-1];
+        }
+        res.add(tmp);
+
+        return res;
+    }
+}
+
+/*
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
         ArrayList<String> res = new ArrayList<>();
         String str = "";
         boolean inRange = false;
@@ -27,3 +58,4 @@ public class Solution {
         return res;
     }
 }
+*/
