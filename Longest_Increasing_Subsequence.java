8ab451fe8b4b08d7a84aec3e1c18c085534c9619
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-30 15:47:52 -0400

    java solution for Longest_Increasing_Subsequence

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -1,6 +1,31 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
         int len = nums.length;
+        int[] res = new int[len+1];
+        res[0] = 0;
+        
+        for (int i = 0; i < len; i++){
+            res[i+1] = 1;
+            for (int j = 0; j < i; j++){
+                if (nums[i] > nums[j]) {
+                    res[i+1] = Math.max(res[i+1], 1+res[j+1]);
+                }
+            }
+        }
+        
+        int max = 0;
+        for (int i : res){
+            max = Math.max(max, i);
+        }
+        
+        return max;
+    }
+}
+
+/*
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
         if (len == 0) return 0;
         int[] arr = new int[len];
         Arrays.fill(arr, 1);
@@ -18,3 +43,4 @@ public class Solution {
         return max;
     }
 }
+*/
