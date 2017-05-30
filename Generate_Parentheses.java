b91b8308c028baa24257b519a1f6234fe210c46d
FelixFanPenn <fankejin@seas.upenn.edu>
2017-05-29 23:24:42 -0400

    java solution for Generate_Parentheses

--- Generate_Parentheses.java
+++ Generate_Parentheses.java
@@ -1,3 +1,34 @@
+/*
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, n, n, "");
+        return list;
+    }
+    
+    public void backtrack(List<String> list, int n, int m, String str){
+        if (m == 0 && n == 0) {
+            list.add(str);
+            return;
+        }
+        if (n == m && n > 0) {
+            str = str + '(';
+            n--;
+            backtrack(list, n, m, str);
+        } else if (n < m && n >= 0){
+            str = str + '(';
+            n--;
+            backtrack(list, n, m, str);
+            n++;
+            m--;
+            str = str.substring(0, str.length()-1) + ')';
+            backtrack(list, n, m, str);
+        }
+    }
+}
+
+*/
+
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
