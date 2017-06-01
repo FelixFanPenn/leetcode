6115e34807b4db2d80264ee380318597d3d6e27a
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-01 18:09:20 -0400

    java solution for Regular_Expression_Matching

--- Regular_Expression_Matching.java
+++ Regular_Expression_Matching.java
@@ -1,3 +1,4 @@
+
 public class Solution {
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) {
@@ -30,3 +31,25 @@ public class Solution {
         return dp[s.length()][p.length()];
     }
 }
+
+
+/*
+public class Solution {
+
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        if (p.length() == 1) {
+            return (s.length() == 1 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
+        }
+        if (p.charAt(1) != '*') {
+            if (s.isEmpty()) return false;
+            return (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p.substring(1));
+        }
+        while (!s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')) {
+            if (isMatch(s, p.substring(2))) return true;
+            s = s.substring(1);
+        }
+        return isMatch(s, p.substring(2));
+    }
+}
+*/
