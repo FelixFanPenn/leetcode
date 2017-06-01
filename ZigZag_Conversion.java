746741b23cd2dd376567b48bc35bcf251f427632
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-01 18:24:08 -0400

    java solution for Zigzag_Conversion

--- ZigZag_Conversion.java
+++ ZigZag_Conversion.java
@@ -1,5 +1,24 @@
 public class Solution {
     public String convert(String s, int numRows) {
+        if (numRows <= 1) return s;
+        String res = "";
+        int size = 2 * numRows - 2;
+        
+        for (int i = 0; i < numRows; i++){
+            for (int j = i; j < s.length(); j += size){
+                res += s.charAt(j);
+                int tmp = j + size - 2 * i;
+                if (i != 0 && i != numRows-1 && tmp < s.length()) {
+                    res += s.charAt(tmp);
+                }
+            }
+        }
+        return res;
+    }
+}
+/*
+public class Solution {
+    public String convert(String s, int numRows) {
         StringBuffer[] sb = new StringBuffer[numRows];
         for (int i = 0; i < numRows; i++) sb[i] = new StringBuffer();
         int i = 0, len = s.length();
@@ -19,3 +38,4 @@ public class Solution {
         return sb[0].toString();
     }
 }
+*/
