cebc0693b31153092b2f2449a31bbe25354ccbdf
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-05 10:45:46 -0400

    java solution for Single_Number_3

---
+++ Single_Number_3.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i : nums){
+            xor ^= i;
+        }
+        
+        xor = xor & (-xor);
+        int[] res = new int[2];
+        for (int i : nums){
+            if ((i & xor) == 0) {
+                res[0] ^= i;
+            } else {
+                res[1] ^= i;
+            }
+        }
+        return res;
+    }
+}
