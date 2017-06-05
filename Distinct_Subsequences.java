5861e56771108b85126eeccd74cd77b946c646a6
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-05 19:31:38 -0400

    java solution for Distinct_Subsequences

--- Distinct_Subsequences.java
+++ Distinct_Subsequences.java
@@ -1,13 +1,11 @@
 public class Solution {
     public int numDistinct(String s, String t) {
         int lens = s.length(), lent = t.length();
-        int[][] res = new int[lens+1][lent+1];  //res[i][j] means how many distinct subseq between s.substring(0, i) and t.substring(0, j)
-
-        for (int i = 1; i <= lens; i++){
-            res[i-1][0] = 1;
-        }
-        for (int i = 2; i <= lent; i++){
-            res[0][i-1] = 0;
+        
+        int[][] res = new int[lens+1][lent+1];
+        
+        for (int i = 0; i <= lens; i++){
+            res[i][0] = 1;
         }
         
         for (int i = 1; i <= lens; i++){
