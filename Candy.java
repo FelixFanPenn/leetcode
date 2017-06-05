8cdb0af36be342af624fc3d1ff3322ee17f9d6c0
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-05 18:39:26 -0400

    java solution for Candy

---
+++ Candy.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        int[] num = new int[ratings.length];
+        Arrays.fill(num, 1);
+        
+        for (int i = 0; i < ratings.length-1; i++){
+            if (ratings[i+1] > ratings[i]) num[i+1] = 1 + num[i];
+        }
+        
+        for (int i = ratings.length-1; i > 0; i--){
+            if (ratings[i-1] > ratings[i]) num[i-1] = Math.max(num[i-1], 1+num[i]);
+        }
+        
+        int sum = 0;
+        for (int i : num){
+            sum += i;
+        }
+        return sum;
+    }
+}
