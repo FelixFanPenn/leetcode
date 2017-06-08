2038f7557695d3eed22614b4e32717d922b55869
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-08 15:54:42 -0400

    java solution for Nim_Game_2

---
+++ Flip_Game_II.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public boolean canWin(String s) {
+        for (int i = 1; i < s.length(); i++){
+            if (s.charAt(i) == '+' && s.charAt(i-1) == '+' && !canWin(s.substring(0, i-1) + "--" + s.substring(i+1))) return true;
+        }
+        return false;
+    }
+}
