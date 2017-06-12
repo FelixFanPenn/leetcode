d42300d156ad8cdff4fd09688e4ed4116501b9a2
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-12 17:22:30 -0400

    java solution for Design_Hit_Counter

---
+++ Design_Hit_Counter.java
@@ -0,0 +1,30 @@
+public class HitCounter {
+    
+    Queue<Integer> q = new LinkedList<>();
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        q.offer(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while (!q.isEmpty() && q.peek() <= timestamp - 300){
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
