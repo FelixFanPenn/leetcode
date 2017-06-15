5434135e4f371f3d736119a6db6a591001dccaf1
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-15 18:10:21 -0400

    java solution for Counting_Bits

--- Counting_Bits.java
+++ Counting_Bits.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int[] countBits(int num) {
+        /*
         int[] res = new int[num+1];
         res[0] = 0;
         int pow = 0;
@@ -8,6 +9,19 @@ public class Solution {
             if (Math.pow(2, pow+1) <= i) pow++;
             res[i] = 1 + res[i - (int)Math.pow(2, pow)];
         }
+        return res;
+        */
+        int[] res = new int[num+1];
+        res[0] = 0;
+        
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0) {
+                res[i] = res[i/2];
+            } else {
+                res[i] = res[i/2] + 1;
+            }
+        }
+        
         return res;
     }
 }
