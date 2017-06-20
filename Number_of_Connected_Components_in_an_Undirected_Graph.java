a52b1ef544c8cd5f92dd81dd57d80f67946e3b8a
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-20 10:01:44 -0400

    Java solution for Number_of_Connected_Components_in_an_Undirected_Graph

--- Number_of_Connected_Components_in_an_Undirected_Graph.java
+++ Number_of_Connected_Components_in_an_Undirected_Graph.java
@@ -1,105 +1,58 @@
 public class Solution {
-    public int countComponents(int n, int[][] edges) {  /// DFS beats 37%
-        if (n <= 1){
-            return n;
-        }   
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++){
-            adj.add(new ArrayList<Integer>());
-        }
-        
-        for (int i = 0; i < edges.length; i++){
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
-        }
-        int count = 0;
-        boolean[] visited = new boolean[n];
-        
-        for (int i = 0; i < n; i++){
-            if (!visited[i]){
-                count++;
-                dfs(visited, adj, i);
-            }
-        }
-        return count;
-    }
-    
-    private void dfs(boolean[] visited, List<List<Integer>> adj, int num){
-        visited[num] = true;
-        for (int i : adj.get(num)){
-            if (!visited[i]){
-                dfs(visited, adj, i);
-            }
-        }
-    }
-    
-    /*   BFS
     public int countComponents(int n, int[][] edges) {
-        if (n <= 1){
-            return n;
-        }   
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++){
-            adj.add(new ArrayList<Integer>());
+        /*
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = i;
+        
+        int res = n;
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(arr, edges[i][0]);
+            int y = find(arr, edges[i][1]);
+            if (x != y) {
+                res--;
+                arr[y] = x;
+            }
         }
         
-        for (int i = 0; i < edges.length; i++){
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
+        return res;
+        */
+        
+        List<List<Integer>> list = new ArrayList<>();
+        int ret = 0;
+        boolean[] res = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<Integer>());
         }
-        int count = 0;
-        boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++){
-            if (!visited[i]){
-                count++;
-                Queue<Integer> queue = new LinkedList<Integer>();
-                queue.offer(i);
-                while (!queue.isEmpty()){
-                    int tmp = queue.poll();
-                    visited[tmp] = true;
-                    for (Integer j : adj.get(tmp)){
-                        if (!visited[j]){
-                            queue.offer(j);
-                        }
-                    }
-                }
+        
+        for (int i = 0; i < edges.length; i++) {
+            list.get(edges[i][0]).add(edges[i][1]);
+            list.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (!res[i]) {
+                dfs(res, list, i);
+                ret++;    
             }
         }
-        return count;
+        return ret;
     }
     
-    */
-    /*   my own version that beats 0.65%
-    public int countComponents(int n, int[][] edges) {
-        Set<Integer> visited = new HashSet<>();
-        int count = 0;
-        for (int i = 0; i < n; i++){
-            if (visited.contains(i)) continue;
-            if (unionFind(edges, i, visited)){
-                count++;
-            }
+    public void dfs(boolean[] res, List<List<Integer>> list, int index) {
+        if (res[index]) return;
+        res[index] = true;
+        for (int i = 0; i < list.get(index).size(); i++) {
+            dfs(res, list, list.get(index).get(i));
         }
-        return count - visited.size() + n;
     }
     
-    private boolean unionFind(int[][] edges, int num, Set<Integer> visited){
-        int next = 0;
-        int cur = 0;
-        boolean found = false;
-        for (int i = 0; i < edges.length; i++){
-            if (num == edges[i][0] || num == edges[i][1]){
-                cur = edges[i][0];
-                next = edges[i][1];
-                visited.add(cur);
-                visited.add(next);
-                edges[i][0] = -1;
-                edges[i][1] = -1;
-                found = true;
-                unionFind(edges, cur, visited);
-                unionFind(edges, next, visited);
-            }
+    /*
+    public int find(int[] arr, int x) {
+        while (arr[x] != x) {
+            x = arr[x];
         }
-        return found;
+        return x;
     }
     */
 }
