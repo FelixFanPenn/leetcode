1ac302bbe3cafd09051ae067f02f888511972d78
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-21 11:46:25 -0400

    Java solution for Merge_Two_Binary_Trees

---
+++ Merge_Two_Binary_Trees.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        int sum = t1.val + t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        t1.val = sum;
+        return t1;
+    }
+    
+}
