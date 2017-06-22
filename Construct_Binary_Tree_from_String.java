720aa4126b4ab9a8d643401da7f8f02bf31b8fd9
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-22 10:08:33 -0400

    Java solution for Construct_Binary_Tree_from_String

---
+++ Construct_Binary_Tree_from_String.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode str2tree(String s) {
+        Stack<TreeNode> s1 = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '(') {
+                i++;
+            } else if (s.charAt(i) == ')') {
+                i++;
+            } else {
+                int j = i+1;
+                while (j < s.length() && s.charAt(j) != '(' && s.charAt(j) != ')') {
+                    j++;
+                }
+                int num = Integer.valueOf(s.substring(i, j));
+                TreeNode node = new TreeNode(num);
+                if (s1.isEmpty()) {
+                    s1.push(node);
+                } else {
+                    if (s1.peek().left == null) {
+                        s1.peek().left = node;
+                    } else {
+                        s1.peek().right = node;
+                    }
+                    s1.push(node);
+                }
+                i = j;
+            }
+        }
+        
+        return s1.isEmpty() ? null : s1.pop();
+    }
+}
