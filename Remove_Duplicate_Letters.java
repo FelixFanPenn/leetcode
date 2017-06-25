d99dd5ce60a5630a7f7912fc589e36d4cdd67906
FelixFanPenn <fankejin@seas.upenn.edu>
2017-06-25 17:22:44 -0400

    Java solution for Remove_Duplicate_Letters

--- Remove_Duplicate_Letters.java
+++ Remove_Duplicate_Letters.java
@@ -1,32 +1,27 @@
 public class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] arr = new int[26];
-        char[] str = s.toCharArray();
-        for (char c : str){
-            arr[c - 'a']++;
-        }
-        
-        Stack<Character> stack = new Stack<>();
-        boolean[] visited = new boolean[26];
-        
-        for (char c : str){
-            arr[c - 'a']--;
-            if (visited[c - 'a']) continue;
-                 // stack is not empty    // the previous char has a same copy after 
-            while(!stack.isEmpty() && arr[stack.peek() - 'a'] > 0 && stack.peek() > c){
-                                                                    // the previous char is larger
-                visited[stack.peek() - 'a'] = false;
-                stack.pop();
-            }
-            
-            stack.push(c);
-            visited[c - 'a'] = true;
-        }
-        
         StringBuilder sb = new StringBuilder();
-        for (char c : stack){
-            sb.append(c);
+        Stack<Character> stack = new Stack<>();
+        int[] chars = new int[256];
+        boolean[] visited = new boolean[256];
+        
+        for (char c : s.toCharArray()) {
+            chars[c]++;
         }
-        return sb.toString();
+        
+        for (char c : s.toCharArray()) {
+            chars[c]--;
+            if (visited[c]) continue;
+            while (!stack.isEmpty() && chars[stack.peek()] > 0 && stack.peek() > c) {
+                visited[stack.pop()] = false;
+            }
+            stack.push(c);
+            visited[c] = true;
+        }
+        
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 }
