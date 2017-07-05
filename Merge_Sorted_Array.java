cb0420a8708d1a3857759668a4ec8932d8da32fd
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-05 17:30:12 -0400

    Java solution for Merge_Sorted_Array

---
+++ Merge_Sorted_Array.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int len = m+n-1;
+        m--; n--;
+        for (int i = len; i >= 0; i--){
+            if (m < 0) {
+                nums1[i] = nums2[n--];
+            } else if (n < 0) {
+                nums1[i] = nums1[m--];
+            } else {
+                if (nums1[m] > nums2[n]){
+                    nums1[i] = nums1[m--];
+                } else {
+                    nums1[i] = nums2[n--];
+                }
+            }
+        }
+    }
+}
