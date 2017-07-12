e1205ffafcdab93622080dfad4213e3bc93af23e
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-12 17:32:53 -0400

    Java solution for Battleships_in_a_Board

---
+++ Battleships_in_a_Board.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int countBattleships(char[][] board) {
+        int res = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.' || (i > 0 && board[i-1][j] == 'X') 
+                    || (j > 0 && board[i][j-1] == 'X')) continue;
+                res++;
+            }
+        }
+        return res;
+    }
+}
