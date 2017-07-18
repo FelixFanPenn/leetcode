49f90bd7ce19a1ecba0fa0299a41d15adb9e707a
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-18 13:44:38 -0400

    Java solution for Lexicographical_Numbers

---
+++ Lexicographical_Numbers.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        
+        for (int i = 1; i < 10; i++) {
+            dfs(res, i, n);
+        }
+        return res;
+    }
+    
+    public void dfs(List<Integer> res, int cur, int n) {
+        if (cur > n) return;
+        res.add(cur);
+        for (int i = 0; i < 10; i++) {
+            if (10 * cur + i > n) return;
+            dfs(res, 10 * cur + i, n);
+        }
+    }
+}
