9329c851c0cb67872c57c73e0fe4573387ac954d
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-24 16:43:56 -0400

    Java solution for Sliding_Window_Maximum

--- Sliding_Window_Maximum.java
+++ Sliding_Window_Maximum.java
@@ -1,34 +1,3 @@
-/*
-public class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return new int[0];
-        
-        int len = nums.length, index = 0;
-        int[] res = new int[len-k+1];
-        Deque<Integer> dq = new ArrayDeque<>();       // arraydeque implementation of deque
-        
-        for (int i = 0; i < len; i++){
-            if (!dq.isEmpty() && i - k + 1 > dq.peek()){
-                dq.poll();
-            }
-            
-            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
-                dq.pollLast();
-            }
-            
-            dq.offer(i);
-            if (i >= k-1){
-                res[index++] = nums[dq.peek()];
-            }
-        }
-        return res;
-    }
-}
-
-*/
-
-
-
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0) return new int[0];
@@ -38,14 +7,19 @@ public class Solution {
         Deque<Integer> dq = new LinkedList<>();      // linked list implementation of deque
         
         for (int i = 0; i < len; i++){
+            // if the first element in current queue is already outside of the range
+            // need to discard it
             if (!dq.isEmpty() && i - k + 1 > dq.peek()){
                 dq.poll();
             }
-            
+
+            //starting from the back, if the number if less than current number, discard them
+            //they cannot be maximum in any window from now on
             while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
                 dq.pollLast();
             }
             
+            // store index but not values
             dq.offer(i);
             if (i >= k-1){
                 res[index++] = nums[dq.peek()];
