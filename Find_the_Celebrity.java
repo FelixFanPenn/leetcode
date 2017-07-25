c3b3e03ded2bb34152ce6a23be012968b4bc5404
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-25 11:19:34 -0400

    Java solution for Find_the_Celebrity

--- Find_the_Celebrity.java
+++ Find_the_Celebrity.java
@@ -5,14 +5,24 @@ public class Solution extends Relation {
     public int findCelebrity(int n) {
         int c = 0;
         for (int i = 1; i < n; i++) {
-            if (knows(c, i)){
+            if (knows(c, i)) {
                 c = i;
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (i != c && (!knows(i, c) || knows(c, i))) return -1;
-        }
         
+        for (int i = 0; i < n; i++) {
+            if (i != c && (knows(c, i) || !knows(i, c))) {
+                return -1;
+            }
+        }
         return c;
     }
 }
+
+/*
+The first loop is to find the candidate as the author explains. 
+In detail, suppose the candidate after the first for loop is person k, 
+it means 0 to k-1 cannot be the celebrity, because they know a previous or current candidate. 
+Also, since k knows no one between k+1 and n-1, k+1 to n-1 can not be the celebrity either. 
+Therefore, k is the only possible celebrity, if there exists one.
+*/
