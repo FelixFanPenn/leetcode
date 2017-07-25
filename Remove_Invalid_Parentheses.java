bd46c0c5b20f1f5b24bfcc50dd70d7026b199a67
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-25 15:56:54 -0400

    Java solution for Remove_Invalid_Parentheses

--- Remove_Invalid_Parentheses.java
+++ Remove_Invalid_Parentheses.java
@@ -1,36 +1,82 @@
 public class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        List<String> res = new ArrayList<>();
-        int len = s.length();
-        boolean found = false;
-        
-        Queue<String> queue = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
-        queue.offer(s);
-        visited.add(s);
-        
-        while(!queue.isEmpty()){
-            String tmp = queue.poll();
-            if (isValid(tmp)){
-                found = true;
-                res.add(tmp);
+        /*
+        Limit max removal rmL and rmR for backtracking boundary. Otherwise it will exhaust all possible valid substrings, not shortest ones.
+        Scan from left to right, avoiding invalid strs (on the fly) by checking num of open parens.
+        If it's '(', either use it, or remove it.
+        If it's '(', either use it, or remove it.
+        Otherwise just append it.
+        Lastly set StringBuilder to the last decision point.
+
+        */
+
+        int rmL = 0, rmR = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') rmL++;
+            else if (s.charAt(i) == ')') {
+                if (rmL == 0) rmR++;
+                else rmL--;
             }
-            
+        }
+        Set<String> res = new HashSet<>();
+        dfs(new StringBuilder(), rmL, rmR, 0, res, s, 0);
+        
+        return new ArrayList<>(res);
+        
+        /* BFS simple but slow
+        List<String> list = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer(s);
+        visited.add(s);
+        boolean found = false;
+         
+        while (!q.isEmpty()) {
+            String tmp = q.poll();
+            if (isValid(tmp)) {
+                list.add(tmp);
+                found = true;
+            }
             if (found) continue;
-            
-            for (int i = 0; i < tmp.length(); i++){   // generate all possible states
-                if (tmp.charAt(i) == '(' || tmp.charAt(i) == ')'){
-                    String newStr = tmp.substring(0, i) + tmp.substring(i+1);
-                    if (!visited.contains(newStr)){  //add it to the queue if not visited
-                        queue.offer(newStr);
-                        visited.add(newStr);
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == '(' || tmp.charAt(i) == ')') {
+                    String str = tmp.substring(0, i) + tmp.substring(i+1);
+                    if (!visited.contains(str)) {
+                        q.offer(str);
+                        visited.add(str);
                     }
                 }
             }
+            
         }
-        return res;
+        return list;
+        */
     }
     
+    public void dfs(StringBuilder sb, int rmL, int rmR, int open, Set<String> set, String s, int len) {
+        if (rmL < 0 || rmR < 0 || open < 0) return;
+        if (len == s.length()) {
+            if (rmL == 0 && rmR == 0 && open == 0) {
+                set.add(sb.toString());
+            }
+            return;
+        }
+        int size = sb.length();
+        char c = s.charAt(len);
+        
+        if (c == '(') {
+            dfs(sb, rmL-1, rmR, open, set, s, len+1);
+            dfs(sb.append(c), rmL, rmR, open+1, set, s, len+1);
+        } else if (c == ')') {
+            dfs(sb, rmL, rmR-1, open, set, s, len+1);
+            dfs(sb.append(c), rmL, rmR, open-1, set, s, len+1);
+        } else {
+            dfs(sb.append(c), rmL, rmR, open, set, s, len+1);
+        }
+        sb.setLength(size);
+    }
+    
+    /*
     private boolean isValid(String str){
         int count = 0;
         for (int i = 0; i < str.length(); i++){
@@ -45,4 +91,5 @@ public class Solution {
         }
         return count == 0;
     }
+    */
 }
