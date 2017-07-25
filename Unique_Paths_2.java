d15ffdc6ff6dba4abb540bc2fb62e96fbcb9bfc4
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-25 09:34:41 -0400

    Java solution for Unique_Paths_2

--- Unique_Paths_2.java
+++ Unique_Paths_2.java
@@ -1,43 +1,18 @@
 public class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row = obstacleGrid.length, col = obstacleGrid[0].length;
-        int[][] res = new int[row][col];
-        boolean flag = true; 
+    public int uniquePathsWithObstacles(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int row = grid.length, col = grid[0].length;
+        int[] res = new int[col];
+        res[0] = 1;
         
-        for (int i = 0; i < row; i++){
-            if (obstacleGrid[i][0] == 1){
-                res[i][0] = 0;
-                flag = false;
-            } else if (flag){
-                res[i][0] = 1;
-            } else {
-                res[i][0] = 0;
-            }
-        }
-        flag = true;
-        
-        for (int j = 0; j < col; j++){
-            if (obstacleGrid[0][j] == 1){
-                res[0][j] = 0;
-                flag = false;
-            } else if (flag){
-                res[0][j] = 1;
-            } else {
-                res[0][j] = 0;
-            }
-        }
-        
-        
-        for (int i = 1; i < row; i++){
-            for (int j = 1; j < col; j++){
-                if (obstacleGrid[i][j] == 1){
-                    res[i][j] = 0;
-                } else {
-                    res[i][j] = res[i][j-1] + res[i-1][j];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) res[j] = 0;
+                else {
+                    if (j > 0) res[j] += res[j-1];
                 }
-                
             }
         }
-        return res[row-1][col-1];
+        return res[col-1];
     }
 }
