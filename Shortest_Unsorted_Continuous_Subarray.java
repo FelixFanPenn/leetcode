64cdbe1b9e93fdbc9f0f903911a9e132c4d34ec8
FelixFanPenn <fankejin@seas.upenn.edu>
2017-07-31 10:01:55 -0400

    Java solution for Shortest_Continuous_Subarray

---
+++ Shortest_Unsorted_Continuous_Subarray.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int len = nums.length, min = nums[len-1], max = nums[0], end = -2, beg = -1;
+        
+        for (int i = 1; i < len; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[len - i - 1]);
+            if (max > nums[i]) end = i;
+            if (min < nums[len - i - 1]) beg = len - i - 1;
+        }
+        
+        return end - beg + 1;
+    }
+}
