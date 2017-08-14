61b28b0ba704e74cf55dcb910b5d7f061fe73d8e
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-13 21:09:01 -0400

    Java solution for Judge_Route_Circle

---
+++ Judge_Route_Circle.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean judgeCircle(String moves) {
+        int l = 0, r = 0, u = 0, d = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'L') {
+                l++;
+            } else if (c == 'R') {
+                r++;
+            } else if (c == 'U') {
+                u++;
+            } else {
+                d++;
+            }
+        }
+        return r == l && u == d;
+    }
+}
