386cca55ff26f21be175a954c8718b50b335da30
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-18 10:31:25 -0400

    Java solution for Binary_Tree_Paths

--- Binary_Tree_Paths.java
+++ Binary_Tree_Paths.java
@@ -1,31 +1,40 @@
 /**
- * Definition for a binary tree node.
+ * Definition of TreeNode:
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
  * }
  */
 public class Solution {
+    /**
+     * @param root the root of the binary tree
+     * @return all root-to-leaf paths
+     */
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> res = new ArrayList<String>();
-        String str = "";
-        help(root, str, res);
+        // Write your code here
+        List<String> res = new ArrayList<>();
+        if (root == null) return res;
+        traverse(root, res, "");
         return res;
     }
     
-    private void help(TreeNode root, String str, List<String> res){
-        if (root == null) return;
-        if (root.left == null && root.right == null) {
-            str = str + (Integer.toString(root.val));
-            res.add(str);
-            return;
-        } else {
-            str = str + (Integer.toString(root.val) + "->");
-            help(root.left, str, res);
-            help(root.right, str, res);
+    public void traverse(TreeNode root, List<String> res, String s) {
+        if (root == null) {
             return;
         }
+        
+        if (root.left == null && root.right == null) {
+            res.add(s + root.val);
+            return;
+        }
+        
+        traverse(root.left, res, s + root.val + "->");
+        
+        traverse(root.right, res, s + root.val + "->");
+        
     }
 }
