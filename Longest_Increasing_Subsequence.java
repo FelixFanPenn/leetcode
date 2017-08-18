fdea224215e5c4c5e1192e1cf8569e9346e3fa54
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-18 15:18:36 -0400

    Java solution for Longest_Increasing_Subsequence

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -1,46 +1,30 @@
-public class Solution {
+class Solution {
+    /*
+    http://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+    
+    
+    
+    */
+    
     public int lengthOfLIS(int[] nums) {
         int len = nums.length;
-        int[] res = new int[len+1];
-        res[0] = 0;
+        int[] tails = new int[len];
+        int size = 0;
         
-        for (int i = 0; i < len; i++){
-            res[i+1] = 1;
-            for (int j = 0; j < i; j++){
-                if (nums[i] > nums[j]) {
-                    res[i+1] = Math.max(res[i+1], 1+res[j+1]);
+        for (int i = 0; i < len; i++) {
+            int l = 0, r = size;
+            while (l != r) {
+                int mid = (l + r) / 2;
+                if (tails[mid] < nums[i]) {
+                    l = mid+1;
+                } else {
+                    r = mid;
                 }
             }
+            tails[l] = nums[i];
+            if (l == size) size++;
         }
         
-        int max = 0;
-        for (int i : res){
-            max = Math.max(max, i);
-        }
-        
-        return max;
+        return size;
     }
 }
-
-/*
-public class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int len = nums.length;
-        if (len == 0) return 0;
-        int[] arr = new int[len];
-        Arrays.fill(arr, 1);
-        int max = 0;
-        
-        for (int i = 0; i < len; i++){
-            for (int j = 0; j < i; j++){
-                if (nums[i] > nums[j]){
-                    arr[i] = arr[i] > arr[j] + 1 ? arr[i] : arr[j] + 1;
-                }
-            }
-            max = Math.max(max, arr[i]);
-        }
-        
-        return max;
-    }
-}
-*/
