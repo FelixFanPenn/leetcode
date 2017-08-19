a57e9a854234d761553f68cb443293df485aeb3f
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-18 22:06:37 -0400

    Java solution for Roman_to_Integer

--- Roman_to_Integer.java
+++ Roman_to_Integer.java
@@ -1,18 +1,41 @@
-public class Solution {
-    private Map<Character, Integer> map =
-      new HashMap<Character, Integer>() {{
-            put('I', 1);  put('V', 5);   put('X', 10);
-            put('L', 50); put('C', 100); put('D', 500);
-            put('M', 1000);
-    }};
-    
+class Solution {
     public int romanToInt(String s) {
-        int sum = 0, pre = 0, cur = 0;
-        for (char c : s.toCharArray()){
-            cur = map.get(c);
-            sum += (cur > pre) ? (cur - 2 * pre) : cur;
-            pre = cur;
+        int sum = 0, len = s.length();
+        int[] arr = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+            switch(s.charAt(i)) {
+                case 'M':
+                    arr[i] = 1000;
+                    break;
+                case 'D':
+                    arr[i] = 500;
+                    break;
+                case 'C':
+                    arr[i] = 100;
+                    break;
+                case 'L':
+                    arr[i] = 50;
+                    break;
+                case 'X':
+                    arr[i] = 10;
+                    break;
+                case 'V':
+                    arr[i] = 5;
+                    break;
+                case 'I':
+                    arr[i] = 1;
+                    break;
+            }
         }
-        return sum;
+        
+        for (int i = 0; i < len-1; i++) {
+            if (arr[i] < arr[i+1]) {
+                sum -= arr[i];
+            } else {
+                sum += arr[i];
+            }
+        }
+        return sum + arr[len-1];
     }
 }
