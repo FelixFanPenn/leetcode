899d4d4dc0860f9fd5e62bdaa31d77b6c8cd7612
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-21 18:10:23 -0400

    Java solution for Kth_Largest_Element_in_an_Array

--- Kth_Largest_Element_in_an_Array.java
+++ Kth_Largest_Element_in_an_Array.java
@@ -1,19 +1,26 @@
-public class Solution {
+class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer first, Integer second){
-                return second.intValue() - first.intValue();
+        return partition(nums, nums.length - k, 0, nums.length-1);
+    }
+    
+    public int partition(int[] nums, int k, int left, int right) {
+        int pivot = nums[right], index = left;
+        
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivot) {
+                swap(nums, index++, i);
             }
-        });
-        
-        for (int i = 0; i < nums.length; i++){
-            queue.offer(nums[i]);
         }
+        swap(nums, index, right);
         
-        for (int i = 0; i < k-1; i++){
-            queue.poll();
-        }
-        return queue.poll();
+        if (index == k) return nums[index];
+        if (index > k) return partition(nums, k, left, index-1);
+        else return partition(nums, k, index+1, right);
+    }
+    
+    public void swap(int[] nums, int l, int r) {
+        int val = nums[l];
+        nums[l] = nums[r];
+        nums[r] = val;
     }
 }
