af2ee15a039339f4fc106f6622d4038c50e873d4
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-22 12:50:26 -0400

    Java solution for Implement_strStr

--- Implement_strStr.java
+++ Implement_strStr.java
@@ -4,27 +4,48 @@ class Solution {
             return -1;
     //generate next array, need O(n) time
         int i = -1, j = 0, m = haystack.length(), n = needle.length();
-        int[] next = new int[n];
-        if (next.length > 0) 
-            next[0] = -1;
-        while (j < n - 1) {
-            if (i == -1 || needle.charAt(i) == needle.charAt(j))
-                next[++j] = ++i;
-            else 
-                i = next[i];
-        }
+        if (n == 0) return 0;
+        int[] next = kmp(needle);
+
     //check through the haystack using next, need O(m) time
         i = 0; j = 0;
         while (i < m && j < n) {
-            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
                 i++;
                 j++;
+            } else {
+                if (j != 0) {
+                    j = next[j-1];
+                } else {
+                    i++;
+                }
             }
-            else 
-                j = next[j];
         }
         if (j == n)
             return i - j;
         return -1;
+    }
+    
+    private int[] kmp(String s){
+        int len = s.length();
+        int[] res = new int[len];
+        char[] ch = s.toCharArray();
+        int i = 0, j = 1;
+        res[0] = 0;
+        while(i < ch.length && j < ch.length){
+            if(ch[j] == ch[i]){
+                res[j] = i+1;
+                i++;
+                j++;
+            }else{
+                if(i == 0){
+                    res[j] = 0;
+                    j++;
+                }else{
+                    i = res[i-1];
+                }
+            }
+        }
+        return res;
     }
 }
