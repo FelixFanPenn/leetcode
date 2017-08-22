294876b36e7c0966f52186a2eae171a05edbdd5c
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-22 11:59:23 -0400

    Java solution for Implement_strStr

--- Implement_strStr.java
+++ Implement_strStr.java
@@ -1,21 +1,30 @@
-public class Solution {
-    public int strStr(String haystack, String needle) {
-        if (needle == null || needle.length() == 0) return 0;
-        if (haystack == null || haystack.length() == 0) return -1;
-        
-        int lenN = needle.length(), lenH = haystack.length();
-        if (lenN > lenH) return -1;
-        
-        for (int i = 0; i < lenH-lenN+1; i++){
-            boolean flag = true;
-            for (int j = 0; j < lenN; j++){
-                if (needle.charAt(j) != haystack.charAt(i+j)){
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) return i;
+class Solution {
+    public int strStr(String haystack, String needle){
+        if (haystack == null || needle == null)
+            return -1;
+    //generate next array, need O(n) time
+        int i = -1, j = 0, m = haystack.length(), n = needle.length();
+        int[] next = new int[n];
+        if (next.length > 0) 
+            next[0] = -1;
+        while (j < n - 1) {
+            if (i == -1 || needle.charAt(i) == needle.charAt(j))
+                next[++j] = ++i;
+            else 
+                i = next[i];
         }
+    //check through the haystack using next, need O(m) time
+        i = 0; j = 0;
+        while (i < m && j < n) {
+            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            }
+            else 
+                j = next[j];
+        }
+        if (j == n)
+            return i - j;
         return -1;
     }
 }
