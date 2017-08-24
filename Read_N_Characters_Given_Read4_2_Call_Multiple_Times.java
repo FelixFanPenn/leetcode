8a58cc7a354c06933515cfdab8bdb03180a54690
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-24 14:22:29 -0400

    Java solution for Read_N_Characters_Given_Read4_2_Call_Multiple_Times

--- Read_N_Characters_Given_Read4_2_Call_Multiple_Times.java
+++ Read_N_Characters_Given_Read4_2_Call_Multiple_Times.java
@@ -7,22 +7,23 @@ public class Solution extends Reader4 {
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
-    public int buffPtr = 0;
-    public int buffCur = 0;
+    
+    int bufferCnt = 0, bufferPtr = 0;
     char[] buff = new char[4];
     
     public int read(char[] buf, int n) {
         int ptr = 0;
         while (ptr < n) {
-            if (buffPtr == 0) buffCur = read4(buff);
+            if (bufferPtr == 0) {
+                bufferCnt = read4(buff);
+            }
+            if (bufferCnt == 0) break;
             
-            if (buffCur == 0) break;
-            
-            while (ptr < n && buffPtr < buffCur) {
-                buf[ptr++] = buff[buffPtr++];
+            while (ptr < n && bufferPtr < bufferCnt) {
+                buf[ptr++] = buff[bufferPtr++];
             }
             
-            if (buffPtr == buffCur) buffPtr = 0;
+            if (bufferCnt == bufferPtr) bufferPtr = 0;
         }
         
         return ptr;
