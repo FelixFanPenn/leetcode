25789e4ac94599831d3a2a4f6f8a072d613cf2c0
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-26 11:59:30 -0400

    Java solution for Product_of_the_Array

---
+++ Product_of_the_Array.java
@@ -0,0 +1,24 @@
+public List<Integer> products(int[] input) {
+	Set<Integer> set = new HashSet<>();
+	Arrays.sort(input);
+	helper(set, input, 1, 0, true);
+	return new ArrayList<>(set);
+}
+
+public void helper(Set<Integer> set, int[] input, int curPro, int pos, boolean first) {
+	if (!first) set.add(curPro);
+	first = false;
+
+	for (int i = pos; i < input.length; i++) {
+		if (i != pos && input[i] == input[i-1]) {
+			continue;
+		}
+
+		if (input[i] == 0) {
+			set.add(0);
+			continue;
+		}
+
+		helper(set, input, curPro * input[i], i+1, first);
+	}
+}
