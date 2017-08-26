73ff91301ef8ecc5de8072168019630c03b5ed80
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-26 15:08:01 -0400

    Java solution for BST_to_Doubly_Linked_List_O(N)

--- BST_to_Doubly_Linked_List.java
+++ BST_to_Doubly_Linked_List.java
@@ -1,1 +1,20 @@
-public 
+// only O(n) TIME
+
+TreeNode root;
+TreeNode head;	// head has the starting element
+TreeNode pre = null;
+
+public BT2DLL(TreeNode root) {
+	if (root == null) return;
+	BT2DLL(root.left);
+
+	if (pre == null) {
+		head = root;
+	} else {
+		root.left = pre;
+		pre.right = root;
+	}
+	pre = root;
+
+	BT2DLL(root.right);
+}
