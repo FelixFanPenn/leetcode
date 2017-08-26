6ba2e92f8f9f2fe732c5c2ca4f527af17d0b1766
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-26 14:39:27 -0400

    Java solution for BST_to_Circular_Linked_List

---
+++ BST_to_Circular_Linked_List.java
@@ -0,0 +1,43 @@
+public TreeNode Tree2LinkedList(TreeNode root){
+	if (root == null) return null;
+	connect(root);
+
+	TreeNode head = root;
+	TreeNode tail = root;
+	while (head.left != null) {
+		head = head.left;
+	}
+
+	while (tail.right != null) {
+		tail = tail.right;
+	}
+	head.left = tail;
+	tail.right = head;
+	return head;
+}
+
+public void connect(TreeNode node) {
+	if (root == null) {
+		return;
+	}
+
+	if (root.left != null) {
+		TreeNode left = root.left;
+		connect(left);
+		while (left.right != null) {
+			left = left.right;
+		}
+		left.right = root;
+		root.left = left;
+	}
+
+	if (root.right != null) {
+		TreeNode right = root.right;
+		connect(right);
+		while (right.left != null) {
+			right = right.left;
+		}
+		right.right = root;
+		root.left = right;
+	}
+}
