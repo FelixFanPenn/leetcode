3427e68cc26aa29fbc2e213ed4ddbea0986f6e18
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-28 17:25:39 -0400

    Java solution for Wildcard_Matching

--- Wildcard_Matching.java
+++ Wildcard_Matching.java
@@ -1,6 +1,8 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
         boolean[][] match=new boolean[s.length()+1][p.length()+1];
+        // match[i][j] means if s.substring(i) and p.substring(j) matches.
+
         match[s.length()][p.length()]=true;
         for(int i=p.length()-1;i>=0;i--){
             if(p.charAt(i)!='*')
@@ -14,6 +16,8 @@ public class Solution {
                         match[i][j]=match[i+1][j+1];
                 else if(p.charAt(j)=='*')
                         match[i][j]=match[i+1][j]||match[i][j+1];
+                        // in this case, if s.substring(i+1) matched p.substring(j)
+                    // or s.substring(i) matched p.substring(j+1), it is true
                 else
                     match[i][j]=false;
             }
