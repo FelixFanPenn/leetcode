52a236f1cc0e0db8855e6ad3118d9deba17ef53e
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-28 16:14:38 -0400

    Java solution for Wildcard_Matching

--- Wildcard_Matching.java
+++ Wildcard_Matching.java
@@ -1,5 +1,30 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
+        boolean[][] match=new boolean[s.length()+1][p.length()+1];
+        match[s.length()][p.length()]=true;
+        for(int i=p.length()-1;i>=0;i--){
+            if(p.charAt(i)!='*')
+                break;
+            else
+                match[s.length()][i]=true;
+        }
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=p.length()-1;j>=0;j--){
+                if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
+                        match[i][j]=match[i+1][j+1];
+                else if(p.charAt(j)=='*')
+                        match[i][j]=match[i+1][j]||match[i][j+1];
+                else
+                    match[i][j]=false;
+            }
+        }
+        return match[0][0];
+    }
+}
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+
         int starIdx = -1, match = 0, sIdx = 0, pIdx = 0;
         while (sIdx < s.length()){
             if (pIdx < p.length() && (s.charAt(sIdx) == p.charAt(pIdx) || p.charAt(pIdx) == '?')) {
@@ -21,5 +46,6 @@ public class Solution {
         while (pIdx < p.length() && p.charAt(pIdx) == '*') pIdx++; 
         
         return pIdx == p.length();
+    
     }
 }
