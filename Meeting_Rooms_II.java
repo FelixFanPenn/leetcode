1de3d3b4d85eab37b1a28ca68bfa766bfd34f02f
FelixFanPenn <fankejin@seas.upenn.edu>
2017-08-30 11:44:24 -0400

    Java solution for Meeting_Rooms_2

--- Meeting_Rooms_II.java
+++ Meeting_Rooms_II.java
@@ -7,6 +7,31 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+    // sort
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+    // PriorityQueue
+        PriorityQueue<Integer> ends = new PriorityQueue<Integer>();
+        ends.offer(intervals[0].end);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start >= ends.peek()) { // no overlap, then should update smallest end.
+                ends.poll();
+            }    
+            ends.offer(intervals[i].end);
+        }
+        return ends.size();
+    }
+}
+
 public class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         int res = 0, len = intervals.length, endpos = 0;
