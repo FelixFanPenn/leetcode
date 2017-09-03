3303652bdf3d52f47ae306addbfd1eab6dc7c6c7
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-03 11:37:39 -0400

    java solution for Dungeon_Game

--- Dungeon_Game.java
+++ Dungeon_Game.java
@@ -1,27 +1,21 @@
-public class Solution {
-    public int calculateMinimumHP(int[][] dun) {
-        int row = dun.length;
-        if (row == 0) return 0;
-        int col = dun[0].length;
+class Solution {
+    public int calculateMinimumHP(int[][] d) {
+        int row = d.length, col = d[0].length;
+        int[][] dp = new int[row][col];
         
-        int[][] health = new int[row][col];
-        health[row-1][col-1] = Math.max(1, 1 - dun[row-1][col-1]);
-        
-        for (int i = row-2; i >= 0; i--) {
-            health[i][col-1] = Math.max(1, health[i+1][col-1] - dun[i][col-1]);
-        }
-        
-        for (int i = col-2; i >= 0; i--) {
-            health[row-1][i] = Math.max(1, health[row-1][i+1] - dun[row-1][i]);
-        }
-        
-        for (int i = row-2; i >= 0; i--) {
-            for (int j = col-2; j >= 0; j--) {
-                int right = Math.max(1, health[i][j+1] - dun[i][j]);
-                int down = Math.max(1, health[i+1][j] - dun[i][j]);
-                health[i][j] = Math.min(right, down);
+        for (int i = row-1; i >= 0; i--) {
+            for (int j = col-1; j >= 0; j--) {
+                if (i == row-1 && j == col-1) {
+                    dp[i][j] = Math.max(1 - d[i][j], 1);
+                } else if (i == row-1) {
+                    dp[i][j] = Math.max(dp[i][j+1] - d[i][j], 1);
+                } else if (j == col-1) {
+                    dp[i][j] = Math.max(dp[i+1][j] - d[i][j], 1);
+                } else {
+                    dp[i][j] = Math.max(1, Math.min(dp[i][j+1] - d[i][j], dp[i+1][j] - d[i][j]));
+                }
             }
         }
-        return health[0][0];
+        return dp[0][0];
     }
 }
