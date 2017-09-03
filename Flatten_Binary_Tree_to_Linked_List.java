d54b477572f3a01d4a25abc7c5bf31cbeb17f0db
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-03 15:24:49 -0400

    java solution for Flatten_Binary_Tree_to_Linked_List

--- Flatten_Binary_Tree_to_Linked_List.java
+++ Flatten_Binary_Tree_to_Linked_List.java
@@ -8,6 +8,22 @@
  * }
  */
 public class Solution {
+	public void flatten(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stk = new Stack<TreeNode>();
+        stk.push(root);
+        while (!stk.isEmpty()){
+            TreeNode curr = stk.pop();
+            if (curr.right!=null)  
+                 stk.push(curr.right);
+            if (curr.left!=null)  
+                 stk.push(curr.left);
+            if (!stk.isEmpty()) 
+                 curr.right = stk.peek();
+            curr.left = null;  // dont forget this!! 
+        }
+    }
+    /*
     private TreeNode pre = null;
     public void flatten(TreeNode root) {   ///post order traversal!!! just just just BRILLIANT
         if (root == null) return;
@@ -17,4 +33,5 @@ public class Solution {
         root.right = pre;
         pre = root;
     }
+	*/
 }
