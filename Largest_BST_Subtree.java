f33f3bb363198c696052b46f7435b0b21daa1ce2
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-03 16:02:22 -0400

    java solution for Largest_BST_Subtree

--- Largest_BST_Subtree.java
+++ Largest_BST_Subtree.java
@@ -7,6 +7,38 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+
+    class Item {
+        int size;
+        int lower;
+        int upper;
+        Item(int s, int l, int u) {
+            size = s;
+            lower = l;
+            upper = u;
+        }
+    }
+    
+    int max = 0;
+    public int largestBSTSubtree(TreeNode root) {
+        if (root == null) return 0;
+        helper(root, null);
+        return max;
+    }
+    
+    public Item helper(TreeNode root, TreeNode parent) {
+        if (root == null) return new Item(0, parent.val, parent.val);
+        Item i1 = helper(root.left, root);
+        Item i2 = helper(root.right, root);
+        if (i1.size == -1 || i2.size == -1 || root.val < i1.upper || root.val > i2.lower) return new Item(-1, 0, 0);
+        int size = 1 + i1.size + i2.size;
+        
+        max = Math.max(max, size);
+        return new Item(size, i1.lower, i2.upper);
+    }
+}
+/*
 public class Solution {
     public int largestBSTSubtree(TreeNode root) {
         if (isValid(root, Integer.MAX_VALUE, Integer.MIN_VALUE)) {
@@ -31,3 +63,4 @@ public class Solution {
         return 1 + count(node.left) + count(node.right);
     }
 }
+*/
