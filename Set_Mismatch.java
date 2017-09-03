dc3f4567743ecc50412b3c96c66cf62adbaf85be
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-03 01:46:16 -0400

    java solution for Set_Mismatch

--- Set_Mismatch.java
+++ Set_Mismatch.java
@@ -1,23 +1,13 @@
-public class Solution {
+class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] res = new int[2];
-        int len = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                res[0] = num;
-            } else {
-                map.put(num, 1);
-            }
+        Set<Integer> set = new HashSet<>();
+        int duplicate = 0, n = nums.length;
+        long sum = (n * (n+1)) / 2;
+        for(int i : nums) {
+            if(set.contains(i)) duplicate = i;
+            sum -= i;
+            set.add(i);
         }
-        
-        for (int i = 0; i <= len; i++) {
-            if (!map.containsKey(i)) {
-                res[1] = i;
-            }
-        }
-        
-        return res;
+        return new int[] {duplicate, (int)sum + duplicate};
     }
 }
