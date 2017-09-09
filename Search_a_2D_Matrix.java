9d6081b7379f137d5d123a7be3f0ad8d1c53b0c8
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-09 14:58:10 -0400

    java solution for Search_A_2D_Matrix

--- Search_a_2D_Matrix.java
+++ Search_a_2D_Matrix.java
@@ -1,41 +1,21 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
         
-        int startRow = 0; int endRow = matrix.length - 1;
-        int mid;
+        int row = matrix.length, col = matrix[0].length;
+        int right = row * col - 1, left = 0;
         
-        while (startRow + 1 < endRow){
-            mid = (startRow + endRow) / 2;
-            if (matrix[mid][0] == target) return true;
-            if (matrix[mid][0] > target) endRow = mid;
-            else startRow = mid;
-        }
-        
-        if (matrix[endRow][0] <= target) {
-            startRow = endRow;
-        } 
-        if (matrix[startRow][0] > target) {
-            return false;
-        }
-        
-        
-        int startColumn = 0; int endColumn = matrix[0].length - 1;
-        
-        while (startColumn + 1 < endColumn){
-            int midColumn = (startColumn + endColumn) / 2;
-            if (matrix[startRow][midColumn] == target){
-                return true;
-            } else if (matrix[startRow][midColumn] > target){
-                endColumn = midColumn;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int r = mid / col;
+            int c = mid % col;
+            if (matrix[r][c] == target) return true;
+            else if (matrix[r][c] > target) {
+                right = mid - 1;
             } else {
-                startColumn = midColumn;
+                left = mid + 1;
             }
         }
-        
-        return matrix[startRow][startColumn] == target || matrix[startRow][endColumn] == target ;
-        
+        return false;
     }
 }
