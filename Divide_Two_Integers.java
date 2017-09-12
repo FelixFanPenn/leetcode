afa6609fbb9247364f2d61606f45a5ca3844af1a
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-12 14:26:51 -0400

    java solution for Divide_Two_Numbers

--- Divide_Two_Integers.java
+++ Divide_Two_Integers.java
@@ -1,26 +1,23 @@
-public class Solution {
+class Solution {
     public int divide(int dividend, int divisor) {
-        int sign1 = (dividend < 0)?-1:1;
-        int sign2 = (divisor < 0)?-1:1;
-        boolean isNegative = (sign1 == sign2) ? false : true;
+        boolean b1 = dividend >= 0;
+        boolean b2 = divisor >= 0;
+        boolean isNeg = b1 ^ b2;
         
-        long newdividend = Math.abs((long)dividend), newdivisor = Math.abs((long)divisor);
-        long ans = 0;
-        
-        while(newdividend >= newdivisor){
-            long tmp = newdivisor;
+        long newDividend = Math.abs((long)dividend);
+        long newDivisor = Math.abs((long)divisor);
+        long res = 0;
+        while (newDividend >= newDivisor) {
             long m = 1;
-            while(newdividend >= (tmp << 1)){
-                m <<= 1;
-                tmp <<= 1;
+            long nd = newDivisor;
+            while (newDividend >= (nd << 1)) {
+                nd = nd << 1;
+                m = m << 1;
             }
-            newdividend -= tmp;
-            ans += m;
+            res += m;
+            newDividend -= nd;
         }
-        return isNegative ? (int) ~ans + 1 : ans > Integer.MAX_VALUE? Integer.MAX_VALUE : (int)ans;
+        
+        return isNeg ? (int)(~res + 1) : res > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) res;
     }
 }
-
-/*
-Finally, we gotta check if the sign is positive or negative. If it is negative, then we apply negation ~result + 1 (two's complement) to get the negative result (why not just result * -1 ? Well, critics might say you use multiplication -_-!!! (lol jk). Also make sure to check if result is overflow, because you know, leetcode is pretty strict to corner cases as well.
-*/
