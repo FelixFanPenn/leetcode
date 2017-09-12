ef49216413330cb3e47e1fdf9c44509118af78de
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-12 12:48:16 -0400

    java solution for Longest_Substring_with_at_Most_Two_Distinct_Characters

--- Longest_Substring_with_at_Most_K_Distinct_Characters.java
+++ Longest_Substring_with_at_Most_K_Distinct_Characters.java
@@ -42,3 +42,34 @@ class Solution {
         return best;
     */
 }
+
+// follow up: if the str is given as a stream therefore cannot track left char
+public class Solution {
+        public int lengthOfLongestSubstringKDistinct(String str, int k) {
+            if (str == null || str.isEmpty() || k == 0) {
+                return 0;
+            }
+            TreeMap<Integer, Character> lastOccurrence = new TreeMap<>();
+            Map<Character, Integer> inWindow = new HashMap<>();
+            int j = 0;
+            int max = 1;
+            for (int i = 0; i < str.length(); i++) {
+                char in = str.charAt(i);
+                while (inWindow.size() == k && !inWindow.containsKey(in)) {
+                    int first = lastOccurrence.firstKey();
+                    char out = lastOccurrence.get(first);
+                    inWindow.remove(out);
+                    lastOccurrence.remove(first);
+                    j = first + 1;
+                }
+                //update or add in's position in both maps
+                if (inWindow.containsKey(in)) {
+                    lastOccurrence.remove(inWindow.get(in));
+                }
+                inWindow.put(in, i);
+                lastOccurrence.put(i, in);
+                max = Math.max(max, i - j + 1);
+            }
+            return max;
+        }
+    }
