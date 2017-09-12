7650fce19ed030121276028394881328b2337597
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-11 22:04:17 -0400

    java solution for Remove_Invalid_Parentheses

--- Remove_Invalid_Parentheses.java
+++ Remove_Invalid_Parentheses.java
@@ -1,15 +1,5 @@
 public class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        /*
-        Limit max removal rmL and rmR for backtracking boundary. Otherwise it will exhaust all possible valid substrings, not shortest ones.
-        Scan from left to right, avoiding invalid strs (on the fly) by checking num of open parens.
-        If it's '(', either use it, or remove it.
-        If it's '(', either use it, or remove it.
-        Otherwise just append it.
-        Lastly set StringBuilder to the last decision point.
-
-        */
-
         int rmL = 0, rmR = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') rmL++;
@@ -22,35 +12,6 @@ public class Solution {
         dfs(new StringBuilder(), rmL, rmR, 0, res, s, 0);
         
         return new ArrayList<>(res);
-        
-        /* BFS simple but slow
-        List<String> list = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
-        Queue<String> q = new LinkedList<>();
-        q.offer(s);
-        visited.add(s);
-        boolean found = false;
-         
-        while (!q.isEmpty()) {
-            String tmp = q.poll();
-            if (isValid(tmp)) {
-                list.add(tmp);
-                found = true;
-            }
-            if (found) continue;
-            for (int i = 0; i < tmp.length(); i++) {
-                if (tmp.charAt(i) == '(' || tmp.charAt(i) == ')') {
-                    String str = tmp.substring(0, i) + tmp.substring(i+1);
-                    if (!visited.contains(str)) {
-                        q.offer(str);
-                        visited.add(str);
-                    }
-                }
-            }
-            
-        }
-        return list;
-        */
     }
     
     public void dfs(StringBuilder sb, int rmL, int rmR, int open, Set<String> set, String s, int len) {
@@ -75,21 +36,77 @@ public class Solution {
         }
         sb.setLength(size);
     }
-    
-    /*
-    private boolean isValid(String str){
-        int count = 0;
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == '(') count++;
-            else if (str.charAt(i) == ')'){
-                if (count != 0){
-                    count--;
-                } else {
-                    return false;
+}
+
+public class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+      List<String> res = new ArrayList<>();
+      
+      // sanity check
+      if (s == null) return res;
+      
+      Set<String> visited = new HashSet<>();
+      Queue<String> queue = new LinkedList<>();
+      
+      // initialize
+      queue.add(s);
+      visited.add(s);
+      
+      boolean reached = false;
+      
+      while (!queue.isEmpty()) {
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            String cur = queue.remove();
+            // Valid
+            if (isValid(cur)) {
+                reached = true;
+                res.add(cur);
+            }
+            // Not Valid Then Delete 
+            if (!reached) {
+                for (int j = 0; j < cur.length(); j++) {
+                    if (cur.charAt(j) != '(' && cur.charAt(j) != ')') continue;
+                    String newStr = cur.substring(0, j) + cur.substring(j + 1);
+                    if (!visited.contains(newStr)) {
+                        queue.add(newStr);
+                        visited.add(newStr);
+                    }
                 }
             }
         }
-        return count == 0;
+        if (reached) break;
     }
-    */
+      
+      return res;
+    }
+    
+    // helper function checks if string s contains valid parantheses
+    boolean isValid(String s) {
+      int count = 0;
+    
+      for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (c == '(') count++;
+        if (c == ')' && count-- == 0) return false;
+      }
+    
+      return count == 0;
+    }
 }
+
+/*
+In BFS we handle the states level by level, in the worst case, we need to handle all the levels, 
+we can analyze the time complexity level by level and add them up to get the final complexity.
+
+On the first level, there's only one string which is the input string s, let's say the length of it is n, 
+to check whether it's valid, we need O(n) time. On the second level, we remove one ( or ) from the first level, 
+so there are C(n, n-1) new strings, each of them has n-1 characters, and for each string, 
+we need to check whether it's valid or not, thus the total time complexity on this level is (n-1) x C(n, n-1). 
+Come to the third level, total time complexity is (n-2) x C(n, n-2), so on and so forth...
+
+Finally we have this formula:
+
+T(n) = n x C(n, n) + (n-1) x C(n, n-1) + ... + 1 x C(n, 1) = n x 2^(n-1).
+
+*/
