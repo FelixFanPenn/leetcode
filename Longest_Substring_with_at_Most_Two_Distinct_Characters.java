9037181dc14e8724819d55b872edae29be017d09
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-12 12:40:24 -0400

    java solution for Longest_Substring_with_at_Most_Two_Distinct_Characters

--- Longest_Substring_with_at_Most_Two_Distinct_Characters.java
+++ Longest_Substring_with_at_Most_Two_Distinct_Characters.java
@@ -24,3 +24,23 @@ public class Solution {
         return len;
     }
 }
+
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int[] count = new int[256];
+        int num = 0, i = 0, res = 0;
+        for (int j = 0; j < s.length(); j++) {
+            count[s.charAt(j)]++;
+            if (count[s.charAt(j)] == 1) num++;
+            if (num > 2) {
+                count[s.charAt(i)]--;
+                while (count[s.charAt(i++)] > 0) {
+                    count[s.charAt(i)]--;
+                }
+                num--;
+            }
+            res = Math.max(res, j - i + 1);
+        }
+        return res;
+    }
+}
