ec8878faa7237ec2b65e1542a716beac828d644d
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-27 15:30:17 -0400

    Clone Graph both recursion and iteration

--- Clone_Graph.java
+++ Clone_Graph.java
@@ -6,6 +6,8 @@
  *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
  * };
  */
+
+//iterative method: O(n) space and O(n) time
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();
@@ -30,3 +32,35 @@ public class Solution {
         return newNode;
     }
 }
+
+
+//iterative method: O(n) space and O(n) time
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label); //new node for return
+        HashMap<Integer, UndirectedGraphNode> map = new HashMap(); //store visited nodes
+        
+        map.put(newNode.label, newNode); //add first node to HashMap
+        
+        LinkedList<UndirectedGraphNode> queue = new LinkedList(); //to store **original** nodes need to be visited
+        queue.add(node); //add first **original** node to queue
+        
+        while (!queue.isEmpty()) { //if more nodes need to be visited
+            UndirectedGraphNode n = queue.pop(); //search first node in the queue
+            UndirectedGraphNode copy = map.get(n.label);
+
+            for (UndirectedGraphNode neighbor : n.neighbors) {
+                if (!map.containsKey(neighbor.label)) { //add to map and queue if this node hasn't been searched before
+                    map.put(neighbor.label, new UndirectedGraphNode(neighbor.label));
+                    queue.add(neighbor);
+                }
+                copy.neighbors.add(map.get(neighbor.label)); //add neighbor to new created nodes
+            }
+        }
+        
+        return newNode;
+    }
+}
+
