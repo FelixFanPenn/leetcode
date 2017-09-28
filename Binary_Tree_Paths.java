aa72609b196df69bf920dbf47c4526035b4396a4
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-27 21:57:42 -0400

    java solution for Binary_Tree_Paths

--- Binary_Tree_Paths.java
+++ Binary_Tree_Paths.java
@@ -38,3 +38,71 @@ public class Solution {
         
     }
 }
+
+//BFS
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) return res;
+        
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<String> q2 = new LinkedList<>();
+        
+        q1.offer(root); q2.offer("");
+        
+        while (!q1.isEmpty()) {
+            TreeNode node = q1.poll();
+            String cur = q2.poll();
+            
+            if (node.left == null && node.right == null) {
+                res.add(cur + node.val);
+            }
+            
+            if (node.left != null) {
+                q1.offer(node.left);
+                q2.offer(cur + node.val + "->");
+            }
+            
+            if (node.right != null) {
+                q1.offer(node.right);
+                q2.offer(cur + node.val + "->");
+            }
+        }
+        
+        return res;
+    }
+}
+
+//DFS
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) return res;
+        
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<String> s2 = new Stack<>();
+        
+        s1.push(root); s2.push("");
+        
+        while (!s1.isEmpty()) {
+            TreeNode node = s1.pop();
+            String cur = s2.pop();
+            
+            if (node.left == null && node.right == null) {
+                res.add(cur + node.val);
+            }
+            
+            if (node.left != null) {
+                s1.push(node.left);
+                s2.push(cur + node.val + "->");
+            }
+            
+            if (node.right != null) {
+                s1.push(node.right);
+                s2.push(cur + node.val + "->");
+            }
+        }
+        
+        return res;
+    }
+}
