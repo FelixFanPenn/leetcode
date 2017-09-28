987f2545df5f236ab97b1a2072de84160673652a
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-27 23:57:16 -0400

    java solution for Populating_Next_Right_Pointers_in_Each_Node_2

--- Populating_Next_Right_Pointers_in_Each_Node_2.java
+++ Populating_Next_Right_Pointers_in_Each_Node_2.java
@@ -9,60 +9,46 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
         if (root == null) return;
-        TreeLinkNode itr = root.next;
-        while (itr != null){
-            if (itr.left != null) {
-                itr = itr.left;
+        
+        TreeLinkNode node = root.next;
+        while (node != null) {
+            if (node.left != null) {
+                node = node.left;
                 break;
             }
             
-            if (itr.right != null) {
-                itr = itr.right;
+            if (node.right != null) {
+                node = node.right;
                 break;
             }
-            
-            itr = itr.next;
+            node = node.next;
         }
         
-        if (root.right != null) root.right.next = itr;
-        if (root.left != null) root.left.next = (root.right == null)? itr : root.right ; 
+        if (root.right != null) root.right.next = node;
+        if (root.left != null) root.left.next = (root.right == null) ? node : root.right;
+        
         connect(root.right);
         connect(root.left);
     }
-}
-
-/*
-public class Solution {
+    
     public void connect(TreeLinkNode root) {
-        TreeLinkNode cur = root;
-        TreeLinkNode head = null, pre = null;//pre ---> the leading node on the next level
-                     //head of the next level
-        while(cur != null){
-            while (cur != null){
-                if(cur.left != null){
-                    if (pre != null){
-                        pre.next = cur.left;
-                    } else {
-                        head = cur.left;
-                    }
-                    pre = cur.left;
-                } 
-                
-                if (cur.right != null){
-                    if (pre != null){
-                        pre.next = cur.right;
-                    } else {
-                        head = cur.right;
-                    }
-                    pre = cur.right;
-                }
-                cur = cur.next;//move to next node
-            }
-            
-            cur = head;  // move to next level
-            pre = null;
-            head = null;
+        TreeLinkNode dummyHead = new TreeLinkNode(0);
+        TreeLinkNode pre = dummyHead;
+        while (root != null) {
+	        if (root.left != null) {
+		        pre.next = root.left;
+		        pre = pre.next;
+	        }
+    	    if (root.right != null) {
+	    	    pre.next = root.right;
+		        pre = pre.next;
+	        }
+    	    root = root.next;
+	        if (root == null) {
+		        pre = dummyHead;
+		        root = dummyHead.next;
+		        dummyHead.next = null;
+	        }
         }
     }
 }
-*/
