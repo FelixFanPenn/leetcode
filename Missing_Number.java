9f876186593ae10417331faf3d62eb7c5790c6da
FelixFanPenn <fankejin@seas.upenn.edu>
2017-09-30 16:03:44 -0400

    java solution for Missing_Number bit manipulation added

--- Missing_Number.java
+++ Missing_Number.java
@@ -8,3 +8,23 @@ public class Solution {
         return sum;
     }
 }
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0, i = 0;
+        
+        for (i = 0; i < nums.length; i++) {
+            res = res ^ i ^ nums[i];
+        }
+        return res ^ i;
+    }
+}
+
+/*
+a^b^b =a, which means two xor operations with the same number 
+will eliminate the number and reveal the original number.
+In this solution, I apply XOR operation to both the index and value of the array. 
+In a complete array with no missing numbers, the index and value 
+should be perfectly corresponding( nums[index] = index), 
+so in a missing array, what left finally is the missing number.
+*/
