48723d99ad0c9e51dbb05267e51e73af6ccf5b39
FelixFanPenn <fankejin@seas.upenn.edu>
2017-10-24 19:27:42 -0400

    java solution for Permutation_in_a_String

--- Permutation_in_String.java
+++ Permutation_in_String.java
@@ -25,3 +25,47 @@ public class Solution {
         return true;
     }
 }
+
+/*
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+    
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c)+1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        
+        int cnt = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s2.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c)-1);
+                if (map.get(c) >= 0) cnt++;
+            }
+        }
+        if (cnt == s1.length()) return true;
+        int left = 0;
+        for (int i = s1.length(); i < s2.length(); i++) {
+            char c1 = s2.charAt(i);
+            char c2 = s2.charAt(left++);
+            if (map.containsKey(c1)) {
+                map.put(c1, map.get(c1)-1);
+                if (map.get(c1) >= 0) cnt++;
+            }
+            if (map.containsKey(c2)) {
+                map.put(c2, map.get(c2)+1);
+                if (map.get(c2) > 0) cnt--;
+            }
+            if (cnt == s1.length()) return true;
+        }
+        
+        return false;
+    }
+}
+*/
